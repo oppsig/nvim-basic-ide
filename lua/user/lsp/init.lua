5e1943e329a60fdb3a89ab79800f4b9aea190930
oppsig <oppsig@users.noreply.github.com>
2022-10-16 12:40:45 +0200

    configure rust tools.

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -6,3 +6,16 @@ end
 require "user.lsp.lsp-installer"
 require("user.lsp.handlers").setup()
 require "user.lsp.null-ls"
+
+local rt = require("rust-tools")
+
+rt.setup({
+  server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
+})
