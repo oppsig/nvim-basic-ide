8cd1c7aa7bd2d858aaaf9f5366410cb7dff7f07a
oppsig <oppsig@users.noreply.github.com>
2022-10-16 12:39:28 +0200

    Add plugin.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -79,6 +79,7 @@ return packer.startup(function(use)
 
   -- LSP
   use { "neovim/nvim-lspconfig", commit = "148c99bd09b44cf3605151a06869f6b4d4c24455" } -- enable LSP
+  use { 'simrat39/rust-tools.nvim'}
   use { "williamboman/nvim-lsp-installer", commit = "e9f13d7acaa60aff91c58b923002228668c8c9e6" } -- simple to use language server installer
   use { "jose-elias-alvarez/null-ls.nvim", commit = "ff40739e5be6581899b43385997e39eecdbf9465" } -- for formatters and linters
   use { "RRethy/vim-illuminate", commit = "c82e6d04f27a41d7fdcad9be0bce5bb59fcb78e5" }
